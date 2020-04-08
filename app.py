6a3ee7a34537cd4af538243e292ebe70fefe0a62
CatButtes <catbuttes@outlook.com>
2020-04-08 16:58:36 +0100

    Check for config file before starting up

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import Tethys
 import yaml
+import pathlib
 
 def main():
-    config_file = open("config.yaml")
-    config = yaml.load(config_file, Loader=yaml.FullLoader)
-    config_file.close()
+    config_file_path = pathlib.Path("config.yaml")
+    config = dict()
+    if config_file_path.exists():
+        config_file = open("config.yaml")
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        config_file.close()
+    else:
+        raise FileNotFoundError("No config found")
 
     bot = Tethys.Tethys(config)
     bot.run()
