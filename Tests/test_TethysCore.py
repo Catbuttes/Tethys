a6786b1d41bcb7317f8670ee8dcae703920327c6
CatButtes <catbuttes@outlook.com>
2020-04-12 21:26:57 +0100

    Test behaviour of on_ready

--- Tests/test_TethysCore.py
+++ Tests/test_TethysCore.py
@@ -1,6 +1,8 @@
 import unittest
 import unittest.mock
+import asyncio
 import Tethys
+from typing import List
 
 
 class MockGuild(object):
@@ -12,9 +14,22 @@ class MockChannel(object):
     def __init__(self):
         self.name = ""
         self.id = -1
+        self.send_count = 0
+        self.send_data: List() = []
+
+    async def send(self, args, **kwargs):
+        self.send_count = self.send_count + 1
+        self.send_data.append(args)
 
 
 class TestTethysCore(unittest.TestCase):
+
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        self.loop.close()
 
     def test_getting_join_log_channel(self):
         testConfig = {"log_channel": 101}
@@ -164,7 +179,7 @@ class TestTethysCore(unittest.TestCase):
         ]
 
         log_channel = testTethys.get_default_log_channel(mockGuild)
-        self.assertTrue(log_channel.id == testConfig["log_channel"])
+        self.assertTrue(log_channel.id == int(testConfig["log_channel"]))
         testTethys.get_channel.assert_called_with(101)
 
     def test_getting_default_via_join_log_channel(self):
@@ -238,3 +253,23 @@ class TestTethysCore(unittest.TestCase):
 
         log_channel = testTethys.get_edit_delete_log_channel(mockGuild)
         self.assertTrue(log_channel.id == tc6.id)
+
+    def test_on_ready(self):
+        testConfig = {"log_channel": 101}
+        testTethys = Tethys.Tethys(testConfig)
+
+        tc0 = MockChannel()
+        tc0.name = "fallback"
+        tc0.id = 101
+
+        testTethys.get_channel = unittest.mock.Mock(return_value=tc0)
+        testTethys.change_presence = unittest.mock.Mock(return_value=asyncio.Future())
+        testTethys.change_presence.return_value.set_result(None)
+        testTethys.application_info = unittest.mock.Mock(return_value=asyncio.Future())
+        testTethys.application_info.return_value.set_result(tc0)
+
+        self.loop.run_until_complete(testTethys.on_ready())
+
+        self.assertEqual(tc0.send_count, 2)
+        self.assertIn("Tethys has just started up", tc0.send_data)
+        self.assertIn("https://discordapp.com/api/oauth2/authorize?client_id=101&permissions=0&scope=bot", tc0.send_data)
