37d039dd09587c8688d1a358cbd9f76ef0b95ef1
CatButtes <catbuttes@outlook.com>
2020-04-12 21:24:47 +0100

    Only accept environment variables for initial conf

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import Tethys
-import yaml
-import pathlib
 import os
 
 
 def main():
-    config_file_path = pathlib.Path("config.yaml")
+
     config = dict()
-    if config_file_path.exists():
-        config_file = open("config.yaml")
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
-        config_file.close()
-    else:
-        config["tethys_token"] = os.environ.get("TETHYS_TOKEN")
-        config["log_channel"] = os.environ.get("TETHYS_LOG_CHANNEL")
+    config["tethys_token"] = os.environ['TETHYS_TOKEN']
+    config["log_channel"] = os.environ['TETHYS_LOG_CHANNEL']
 
     bot = Tethys.Tethys(config)
     bot.run()
